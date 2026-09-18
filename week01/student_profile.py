6487607ce290a47e7b607a1686d0644f8c982cf5
cerenkrdmiir <cerenkrdmiir@icloud.com>
2026-09-18 18:04:01 +0300

    Add student profile input and display functionality

---
+++ week01/student_profile.py
@@ -0,0 +1,8 @@
+name = input("enter your name :")
+department = input("enter your department :")
+age = input("enter your age :")
+career_goal = input("enter your career goal :")
+
+
+print("___student profile___")
+print(f"name : {name} department :{department} age :{age} career goal: {career_goal}")
